2e5e145b1e01e61c28fc2503ff40b36b38556967
Denys Dovgan <131907373+DenysDovgan@users.noreply.github.com>
2025-03-10 19:58:25 +0100

    avg method implemented

--- Program.cs
+++ Program.cs
@@ -4,3 +4,14 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("mod1");
 Console.WriteLine("mod2");
 Console.WriteLine("mod3");
+
+static void printAvg(int[] arr)
+{
+    float avg = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        avg += arr[i];
+    }
+    avg /= arr.Length;
+    Console.WriteLine("Average: " + avg);
+}
