09c8435d3e26f279391014fe3e1057233360e56e
Denys Dovgan <131907373+DenysDovgan@users.noreply.github.com>
2025-03-10 19:55:38 +0100

    Revert "Avg method implemented"
    This reverts commit a1e2c68a104b58e184974e6fa77e75ed8916b92c.

--- Program.cs
+++ Program.cs
@@ -4,14 +4,3 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("mod1");
 Console.WriteLine("mod2");
 Console.WriteLine("mod3");
-
-static void printAverage(int[] arr)
-{
-    int avg = 0;
-    for (int i = 0; i < arr.Length; i++)
-    {
-        avg += arr[i];
-    }
-    avg = avg / arr.Length;
-    Console.WriteLine("Average: " + avg);
-}
